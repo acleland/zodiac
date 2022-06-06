d532f8e752b7e360ebeca6d2968eb77d641524bf
Andrea Cleland <andreaclelan@gmail.com>
2022-06-06 16:31:58 -0700

    TDD - wrote test first

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const zodiac = require('../data/zodiac');
 
 describe('zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return a list of zodiac signs, only their id and name', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((sign) => ({ id: sign.id, name: sign.name }));
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
