7b3225baca4aaeed0267f506245e84ca589cb16d
Andrea Cleland <andreaclelan@gmail.com>
2022-06-06 17:42:34 -0700

    implemented /zodiac route, first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const zodiac = require('../data/zodiac');
+const { zodiac } = require('../data/zodiac');
 
 describe('zodiac routes', () => {
   beforeEach(() => {
